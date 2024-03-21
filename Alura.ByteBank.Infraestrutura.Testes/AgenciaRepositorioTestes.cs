b88df1e78d2a19aaa5bf033499287ce5860f1ad4
Rafael Menezes <rafa.rcm@hotmail.com>
2024-03-21 15:29:13 -0300

    Testando outras operações do CRUD

---
+++ Alura.ByteBank.Infraestrutura.Testes/AgenciaRepositorioTestes.cs
@@ -0,0 +1,56 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class AgenciaRepositorioTestes
+    {
+        private IAgenciaRepositorio _repositorio;
+
+        public AgenciaRepositorioTestes()
+        {
+            _repositorio = new AgenciaRepositorio();
+        }
+
+        [Fact]
+        public void TestaObterTodasAgencias()
+        {
+            //Arrange
+            //Act
+            List<Agencia> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+        }
+
+        [Fact]
+        public void TestaObterAgenciaPorId()
+        {
+            //Arrange
+            //Act
+            var agencia = _repositorio.ObterPorId(1);
+
+            //Assert
+            Assert.NotNull(agencia);
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void TestaObterAgenciasPorVariosId(int id)
+        {
+            //Arrange
+            //Act
+            var agencia = _repositorio.ObterPorId(id);
+
+            //Assert
+            Assert.NotNull(agencia);
+
+        }
+    }
+}
